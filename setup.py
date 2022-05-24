dddea0b039b7c39c5faf7ee82a9894e5c25aa3e8
nalinigans <nalini@omicsautomation.com>
2022-05-24 11:16:00 -0700

    Change all tabs to spaces in setup.py and allow flake8 to guide coding style

--- setup.py
+++ setup.py
@@ -1,26 +1,24 @@
-from __future__ import absolute_import, print_function 
-
 from setuptools import setup, Extension, find_packages
-from pkg_resources import resource_filename
 
 import os
 import shutil
 import glob
 import sys
-import logging
 
 # Directory where a copy of the CPP compiled object is found.
-GENOMICSDB_LOCAL_DATA_DIR = 'genomicsdb_data'
+GENOMICSDB_LOCAL_DATA_DIR = "genomicsdb_data"
 
-# Specify genomicsdb install location via "--with-genomicsdb=<genomicsdb_install_path>" command line arg
-GENOMICSDB_INSTALL_PATH = os.getenv('GENOMICSDB_HOME', default = '/usr/local')
+# Specify genomicsdb install location via
+#     "--with-genomicsdb=<genomicsdb_install_path>" command line arg
+GENOMICSDB_INSTALL_PATH = os.getenv("GENOMICSDB_HOME", default="/usr/local")
+print("GENOMICSDB_INSTALL_PATH={}", GENOMICSDB_INSTALL_PATH)
 args = sys.argv[:]
 for arg in args:
-	if arg.find('--with-genomicsdb=') == 0:
-		GENOMICSDB_INSTALL_PATH = os.path.expanduser(arg.split('=')[1])
-		sys.argv.remove(arg)
+    if arg.find("--with-genomicsdb=") == 0:
+        GENOMICSDB_INSTALL_PATH = os.path.expanduser(arg.split("=")[1])
+        sys.argv.remove(arg)
 
-print('Compiled GenomicsDB Install Path: {}'.format(GENOMICSDB_INSTALL_PATH))
+print("Compiled GenomicsDB Install Path: {}".format(GENOMICSDB_INSTALL_PATH))
 
 GENOMICSDB_INCLUDE_DIR = os.path.join(GENOMICSDB_INSTALL_PATH, "include")
 GENOMICSDB_LIB_DIR = os.path.join(GENOMICSDB_INSTALL_PATH, "lib")
@@ -28,72 +26,73 @@ GENOMICSDB_LIB_DIR = os.path.join(GENOMICSDB_INSTALL_PATH, "lib")
 rpath = []
 link_args = []
 for arg in args:
-	if arg.find('--with-libs') == 0:
-		glob_paths = [os.path.join(GENOMICSDB_LIB_DIR, e) for e in ['lib*genomicsdb*.so','lib*genomicsdb*.dylib']]
-		lib_paths = []
-		for paths in glob_paths:
-			lib_paths.extend(glob.glob(paths))
-		print('Adding the following libraries to the GenomicsDB Package :')
-		print(*lib_paths, sep="\n")
+    if arg.find("--with-libs") == 0:
+        glob_paths = [
+            os.path.join(GENOMICSDB_LIB_DIR, e)
+            for e in ["lib*genomicsdb*.so", "lib*genomicsdb*.dylib"]
+        ]
+        lib_paths = []
+        for paths in glob_paths:
+            lib_paths.extend(glob.glob(paths))
+        print("Adding the following libraries to the GenomicsDB Package :")
+        print(*lib_paths, sep="\n")
 
-		dst = os.path.join(GENOMICSDB_LOCAL_DATA_DIR, 'lib')
-		if not os.path.exists(dst):
-			os.makedirs(dst)
-		for lib_path in lib_paths:
-			print('Copying {0} to {1}'.format(lib_path, dst))
-			shutil.copy(lib_path, dst)
-		if sys.platform == 'darwin':
-			link_args = ["-Wl,-rpath," + dst]
-		else:
-			rpath = ['$ORIGIN/' + dst]
-		sys.argv.remove(arg)
+        dst = os.path.join(GENOMICSDB_LOCAL_DATA_DIR, "lib")
+        if not os.path.exists(dst):
+            os.makedirs(dst)
+        for lib_path in lib_paths:
+            print("Copying {0} to {1}".format(lib_path, dst))
+            shutil.copy(lib_path, dst)
+        if sys.platform == "darwin":
+            link_args = ["-Wl,-rpath," + dst]
+        else:
+            rpath = ["$ORIGIN/" + dst]
+        sys.argv.remove(arg)
 
-genomicsdb_extension=Extension(
-	"genomicsdb",
-	language="c++",
-	sources=["src/genomicsdb.pyx", "src/genomicsdb_processor.cpp"],
-	libraries=["tiledbgenomicsdb"],
-	include_dirs=[GENOMICSDB_INCLUDE_DIR],
-	library_dirs=[GENOMICSDB_LIB_DIR],
-	runtime_library_dirs=rpath,
-	extra_link_args=link_args,
-	extra_compile_args=["-std=c++11"]
+genomicsdb_extension = Extension(
+    "genomicsdb",
+    language="c++",
+    sources=["src/genomicsdb.pyx", "src/genomicsdb_processor.cpp"],
+    libraries=["tiledbgenomicsdb"],
+    include_dirs=[GENOMICSDB_INCLUDE_DIR],
+    library_dirs=[GENOMICSDB_LIB_DIR],
+    runtime_library_dirs=rpath,
+    extra_link_args=link_args,
+    extra_compile_args=["-std=c++11"],
 )
 
-with open('README.md') as f:
-	long_description = f.read()
+with open("README.md") as f:
+    long_description = f.read()
 
 setup(
-	name='genomicsdb',
-	description='Experimental Python Bindings for querying GenomicsDB',
-	long_description=long_description,
-	long_description_content_type='text/markdown',
-	author='GenomicsDB.org',
-	author_email='support@genomicsdb.org',
-	maintainer='GenomicsDB.org',
-	maintainer_email='support@genomicsdb.org',
-	license='MIT',
-	ext_modules=[genomicsdb_extension],
-	setup_requires=['cython>=0.27'],
-	install_requires=[
-		'numpy>=1.7'],
-	python_requires='>=3.7',
-	packages = find_packages(exclude=["package", "test"]),
-	keywords=['genomics', 'genomicsdb', 'variant', 'vcf', 'variant calls'],
-	include_package_data=True,
-	version = '0.0.8.0',
-	classifiers=[
-		'Development Status :: 3 - Alpha',
-		'Intended Audience :: Developers',
-		'Intended Audience :: Information Technology',
-		'Intended Audience :: Science/Research',
-		'License :: OSI Approved :: MIT License',
-		'Topic :: Software Development :: Libraries :: Python Modules',
-		'Operating System :: POSIX :: Linux',
-		'Operating System :: MacOS :: MacOS X',
-		'Programming Language :: Python :: 3.7',
-		'Programming Language :: Python :: 3.8',
-		'Programming Language :: Python :: 3.9',
-	],
+    name="genomicsdb",
+    description="Experimental Python Bindings for querying GenomicsDB",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="GenomicsDB.org",
+    author_email="support@genomicsdb.org",
+    maintainer="GenomicsDB.org",
+    maintainer_email="support@genomicsdb.org",
+    license="MIT",
+    ext_modules=[genomicsdb_extension],
+    setup_requires=["cython>=0.27"],
+    install_requires=["numpy>=1.7"],
+    python_requires=">=3.7",
+    packages=find_packages(exclude=["package", "test"]),
+    keywords=["genomics", "genomicsdb", "variant", "vcf", "variant calls"],
+    include_package_data=True,
+    version="0.0.8.0",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Information Technology",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Operating System :: POSIX :: Linux",
+        "Operating System :: MacOS :: MacOS X",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
 )
-
