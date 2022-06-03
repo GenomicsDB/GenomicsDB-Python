0c8b40a72ba3954dd159ddb3a8a7a1c10cbe7b3d
Nalini Ganapati <35076948+nalinigans@users.noreply.github.com>
2022-06-03 12:20:08 -0700

    Build TensorFlow/Arrow/... compatible wheels on Centos 6 without including OpenSSL/CURL/uuid (#13)

--- package/scripts/install_python.sh
+++ package/scripts/install_python.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+# Argument parsing
+PYTHON_USER=$1
+PYTHON_USER_ID=$2
+PYTHON_GROUP_ID=$3
+
 PYTHON_MAJOR=3
-OPENSSL_VERSION=1.0.2o
+OPENSSL_VERSION=1.1.1o
 
 die() {
   if [[ $# -eq 1 ]]; then
@@ -21,7 +26,7 @@ check_rc() {
 install_python_version() {
   # Download and extract source
   VERSION=$1
-  wget -nv https://www.python.org/ftp/python/$VERSION/Python-$VERSION.tgz &&
+  wget $WGET_NO_CERTIFICATE -nv https://www.python.org/ftp/python/$VERSION/Python-$VERSION.tgz &&
     tar -xvzf Python-$VERSION.tgz
   check_rc $?
   pushd Python-$VERSION
@@ -36,7 +41,7 @@ install_python_version() {
 
 install_openssl() {
   pushd /tmp
-  wget -nv https://www.openssl.org/source/openssl-$OPENSSL_VERSION.tar.gz &&
+  wget $WGET_NO_CERTIFICATE -nv https://www.openssl.org/source/openssl-$OPENSSL_VERSION.tar.gz &&
     tar xzvf openssl-$OPENSSL_VERSION.tar.gz &&
     pushd openssl-$OPENSSL_VERSION
     CFLAGS=-fPIC ./config -fPIC -shared --prefix=/usr/local/ssl-$OPENSSL_VERSION &&
@@ -75,8 +80,25 @@ sanity_test_python() {
 
 install
 
+# Workaround for Centos 6 being EOL'ed
+curl https://www.getpagespeed.com/files/centos6-eol.repo --output /etc/yum.repos.d/CentOS-Base.repo
+yum -y install centos-release-scl
+curl https://www.getpagespeed.com/files/centos6-scl-eol.repo --output /etc/yum.repos.d/CentOS-SCLo-scl.repo
+curl https://www.getpagespeed.com/files/centos6-scl-rh-eol.repo --output /etc/yum.repos.d/CentOS-SCLo-scl-rh.repo
+sed -i 's/http/https/g' /etc/yum.repos.d/CentOS-Base.repo
+sed -i 's/http/https/g' /etc/yum.repos.d/CentOS-SCLo-scl.repo
+sed -i 's/http/https/g' /etc/yum.repos.d/CentOS-SCLo-scl-rh.repo
+WGET_NO_CERTIFICATE="--no-check-certificate"
+
 yum install -y zlib-devel bzip2-devel libffi libffi-devel wget
 yum groupinstall -y "development tools"
+
+if [[ $PYTHON_USER_ID != 0  && $PYTHON_GROUP_ID != 0 ]]; then
+  echo "groupadd -g $PYTHON_GROUP_ID genomicsdb-python"
+  echo "useradd -m $PYTHON_USER -u $PYTHON_USER_ID -g $PYTHON_GROUP_ID"
+  groupadd -g $PYTHON_GROUP_ID genomicsdb-python &&
+  useradd -m $PYTHON_USER -u $PYTHON_USER_ID -g $PYTHON_GROUP_ID
+fi
 
 install_devtoolset &&
   install_openssl &&
