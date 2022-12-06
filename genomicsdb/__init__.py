ea272663ecc5634532607583a76cf86f41024103
Nalini Ganapati <nganapati@gmail.com>
2022-12-06 13:25:18 -0800

    Get python to build locally and the created genomicsdb package installable in Python 3.10

--- genomicsdb/__init__.py
+++ genomicsdb/__init__.py
@@ -3,9 +3,9 @@ import os
 import sys
 
 if sys.platform == "darwin":
-    ctypes.CDLL(os.path.join("lib", "libtiledbgenomicsdb.dylib"))
+    ctypes.CDLL(os.path.join(os.path.dirname(os.path.abspath(__file__)), "lib/libtiledbgenomicsdb.dylib"))
 else:
-    ctypes.CDLL(os.path.join("lib", "libtiledbgenomicsdb.so"))
+    ctypes.CDLL(os.path.join(os.path.dirname(os.path.abspath(__file__)), "lib/libtiledbgenomicsdb.so"))
 
 __path__ = __import__("pkgutil").extend_path(__path__, __name__)
 from .genomicsdb import *
