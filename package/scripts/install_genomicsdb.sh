3580047818444b51d968549317f5714f63d56421
Enes Kuluk <54481799+eneskuluk@users.noreply.github.com>
2023-10-23 14:34:33 -0700

    add some dependencies back

--- package/scripts/install_genomicsdb.sh
+++ package/scripts/install_genomicsdb.sh
@@ -38,6 +38,21 @@ INSTALL_PREFIX=/usr/local
 
 OPENSSL_VERSION=${OPENSSL_VERSION:-3.0.11}
 
+echo "Installing minimal dependencies..."
+yum install -y centos-release-scl && yum install -y devtoolset-11 &&
+  yum install -y -q deltarpm &&
+  yum update -y -q &&
+  yum install -y -q epel-release &&
+  yum install -y -q which wget git &&
+  yum install -y -q autoconf automake libtool unzip &&
+  yum install -y -q cmake3 patch &&
+  yum install -y -q perl perl-IPC-Cmd &&
+  yum install -y -q libuuid libuuid-devel &&
+  yum install -y -q curl libcurl-devel &&
+  echo "Installing minimal dependencies DONE"
+
+source /opt/rh/devtoolset-11/enable
+
 
 echo "git clone https://github.com/GenomicsDB/GenomicsDB.git -b $BRANCH GenomicsDB"
 git clone https://github.com/GenomicsDB/GenomicsDB.git -b $BRANCH GenomicsDB
