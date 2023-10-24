8cabbdb976ccdcf4adaab4f4eeb1ba868681f0f9
Enes Kuluk <54481799+eneskuluk@users.noreply.github.com>
2023-10-24 13:40:18 -0700

    install dependencies

--- package/publish_package.sh
+++ package/publish_package.sh
@@ -77,6 +77,9 @@ if [[ $RC != 0 ]]; then
   exit $RC
 fi
 
+python -m pip install --upgrade pip
+python -m pip install -r requirements_dev.txt
+python -m pip install -r requirements.txt
 # Run setup for source distribution of genomicsdb api and binary distribution of protobuf bindings
 python setup.py sdist
 
