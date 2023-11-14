130efeaf6e6692a5cfb34838df21d691e24a5ad7
Enes <54481799+eneskuluk@users.noreply.github.com>
2023-11-13 16:24:22 -0800

    path changes in mac side

--- install_genomicsdbformac.sh
+++ install_genomicsdbformac.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
-INSTALL_PREFIX=/usr/local
+PREFIX_DIR=${PREFIX_DIR:-$(mktemp -d)}
+GENOMICSDB_DIR=$PREFIX_DIR/GenomicsDB
+GENOMICSDB_HOME=$GENOMICSDB_DIR/release
+export $GENOMICSDB_HOME
 export OPENSSL_ROOT_DIR=$(brew --prefix openssl@3)
-git clone https://github.com/GenomicsDB/GenomicsDB.git -b develop GenomicsDB-build
-cd GenomicsDB-build
+rm -fr $GENOMICSDB_DIR
+git clone https://github.com/GenomicsDB/GenomicsDB.git -b develop $GENOMICSDB_DIR
+pushd $GENOMICSDB_DIR
 echo "Starting GenomicsDB build"
 mkdir build && cd build &&
-cmake -DCMAKE_INSTALL_PREFIX=$GENOMICSDB_HOME -DPROTOBUF_ROOT_DIR=./protobuf-install -DAWSSDK_ROOT_DIR=./aws-install -DGCSSDK_ROOT_DIR=./gcs-install -DBUILD_EXAMPLES=False -DDISABLE_MPI=True -DDISABLE_OPENMP=True -DUSE_HDFS=False .. &&
+cmake -DCMAKE_INSTALL_PREFIX=$GENOMICSDB_HOME -DBUILD_EXAMPLES=False -DDISABLE_MPI=True -DDISABLE_OPENMP=True -DUSE_HDFS=False .. &&
 	make -j4 && make install
-cd ../..
-  if [[ -f $INSTALL_PREFIX/lib/libtiledbgenomicsdb.dylib ]]; then
-    echo "Building GenomicsDB in $GENOMICSDB_DIR DONE"
+popd
+  if [[ -f $GENOMICSDB_HOME/lib/libtiledbgenomicsdb.dylib ]]; then
+    echo "Building GenomicsDB in $$GENOMICSDB_DIR DONE"
   else
     echo "Something wrong with building GenomicsDB at $GENOMICSDB_HOME"
     exit 1
