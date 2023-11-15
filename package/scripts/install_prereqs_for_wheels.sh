9aeaf280326071624512b531a69cb1cd8d245947
Enes <54481799+eneskuluk@users.noreply.github.com>
2023-11-15 14:08:45 -0800

    typo fix

--- package/scripts/install_prereqs_for_wheels.sh
+++ package/scripts/install_prereqs_for_wheels.sh
@@ -68,7 +68,7 @@ install_genomicsdb_for_centos7() {
     export OPENSSL_ROOT_DIR=$INSTALL_PREFIX
     export LD_LIBRARY_PATH=$INSTALL_PREFIX/lib64:$INSTALL_PREFIX/lib:$LD_LIBRARY_PATH
     git clone https://github.com/GenomicsDB/GenomicsDB.git -b develop GenomicsDB-build
-    push GenomicsDB-build
+    pushd GenomicsDB-build
     echo "Starting GenomicsDB build"
     mkdir build && cd build &&
     cmake -DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX -DBUILD_EXAMPLES=False -DDISABLE_MPI=True -DDISABLE_OPENMP=True -DUSE_HDFS=False .. &&
