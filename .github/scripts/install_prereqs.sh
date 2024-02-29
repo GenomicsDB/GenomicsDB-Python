f7bced20b3fb9eef8e318768bc9a8a9a200c4f55
Nalini Ganapati <nalini@datma.com>
2024-02-29 14:03:48 -0800

    Dependencies

--- .github/scripts/install_prereqs.sh
+++ .github/scripts/install_prereqs.sh
@@ -178,8 +178,8 @@ if [[ $1 == "release" ]]; then
   # Interested only in static openssl/curl/uuid libraries for a wheels release
   mkdir build &&
     pushd build &&
-    cmake .. -DCMAKE_PREFIX_PATH=$INSTALL_PREFIX -DBUILD_EXAMPLES=False -DDISABLE_MPI=True -DDISABLE_OPENMP=True -DUSE_HDFS=False -DOPENSSL_USE_STATIC_LIBS=True &&
-    make -j4 && rm -fr dependencies/TileDB && make -j4 &&
+    cmake .. -DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX -DCMAKE_PREFIX_PATH=$INSTALL_PREFIX -DBUILD_EXAMPLES=False -DDISABLE_MPI=True -DDISABLE_OPENMP=True -DUSE_HDFS=False -DOPENSSL_USE_STATIC_LIBS=True &&
+    make -j4 && rm -fr dependencies/TileDB && make clean && make -j4 &&
     $SUDO make install &&
     popd
   popd
