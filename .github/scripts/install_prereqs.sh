80348008c0d0190403aeabb66d02e01bcdbfe88c
Nalini Ganapati <nalini@datma.com>
2024-06-19 14:49:45 -0700

    Debug

--- .github/scripts/install_prereqs.sh
+++ .github/scripts/install_prereqs.sh
@@ -118,7 +118,7 @@ install_prereqs_for_centos7() {
     export CXX=/opt/rh/devtoolset-11/root/usr/bin/g++ &&
     ls -l /opt/rh/devtoolset-11/root/usr/bin/gcc &&
     ls -l /opt/rh/devtoolset-11/root/usr/bin/g++ &&
-    echo "DONE"
+    echo "Installing devtoolset DONE"
   if [[ $1 == "release" ]]; then
     install_openssl3
     install_curl
